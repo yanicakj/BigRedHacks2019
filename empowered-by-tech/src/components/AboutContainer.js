2c7ad3fdcdb9e4bde27357d0581f7a0b7cd31ce7
Alyssa Maquiling <alymaquiling@gmail.com>
2019-09-22 07:11:06 -0400

    update

--- empowered-by-tech/src/components/AboutContainer.js
+++ empowered-by-tech/src/components/AboutContainer.js
@@ -24,12 +24,12 @@ class AboutContainer extends Component {
                     neighborhoods. By providing toolkits to help communities organize these "block parties", 
                     they have been incredibly successful in their mission of forming local support groups to share
                     local news, information, and resources to better prepare everyone for unpredictable events
-                    before they occur, and without taking a fear-based approach.
+                    before they occur without the need for a fear-based approach.
                     <br/>
                     <br/>
                     On our social platform, members of a community can post announcements, 
                     share local events, and offer resources to others in need. 
-                    EmpoweredBy.Tech was created with these people in mind. We believe that we can 
+                    EmpoweredByTech was created with these people in mind. We believe that we can 
                     reduce the societal barriers even further by taking advantage of more recent
                     technological advancements to make these support groups even more accessible.
                 </Typography>
--- empowered-by-tech/src/components/BoardContainer.js
+++ empowered-by-tech/src/components/BoardContainer.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react'; 
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Button, Divider, List } from '@material-ui/core';
+import Add from '@material-ui/icons/Add';
 
 class BoardContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            announcements: []
+        };
     }
 
     render () {
         return (
+            <div style={{alignSelf:'right'}}>
+                <br />
+                
             <Container>
+                
                 <br />
-                <br />
-                <Typography variant='h3' align='left'>
+                <Typography variant='h3'>
                     Community Board
                 </Typography>
-                
+                <br />
+                <Divider />
+                <br />
+                <span><Button variant='contained' style={{backgroundColor:'#b31b1b', color:'white',borderColor:'black'}}>
+                    <Add  />
+                    Add a post
+                </Button></span>
+                <List>
+
+                </List>
                 
             </Container>
+            </div>
         );
     }
 }
